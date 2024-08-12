65a957ce47a984f90e891ee8c379e6bd9c4e82da
Omar Campos <omarjc-1997@hotmail.com>
2024-08-12 12:30:32 -0500

    feat: add git ignore

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
 
 
 app = FastAPI()
+
+router = APIRouter()
+
+
+@router.get("/ping")
+def ping():
+    return {"ping": "pong"}
+
+
+app.include_router(router, prefix="/api/v1")
